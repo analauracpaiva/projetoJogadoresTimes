6956ab4b05df0fe28117e027f47cdd0a1ba3fdbb
analauracpaiva <99214634+analauracpaiva@users.noreply.github.com>
2022-07-15 08:36:00 -0300

    JogadoresView.vue

--- src/views/JogadoresView.vue
+++ src/views/JogadoresView.vue
@@ -1,4 +1,128 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      jogadores: [],
+      jogador: {},
+      times: [],
+    };
+  },
+  async created() {
+    await this.buscarTodosOsJogadores();
+    await this.buscarTodosOsTimes();
+  },
+  methods: {
+    async buscarTodosOsTimes() {
+      const times = await axios.get("http://localhost:4000/times");
+      this.times = times.data;
+    },
+    async buscarTodosOsJogadores() {
+      const resposta = await axios.get(
+        "http://localhost:4000/jogadores?expand=time"
+      );
+      this.jogadores = resposta.data;
+    },
+    async salvar() {
+      await axios.post("http://localhost:4000/jogadores", this.jogador);
+      await this.buscarTodosOsJogadores();
+    },
+  },
+};
+</script>
+
 <template>
-  <main>Jogadores</main>
-  <footer>Rodapé</footer>
+  <div class="container">
+    <div class="title">
+      <h2>Gerenciamento de Jogadores</h2>
+    </div>
+    <div class="form-input">
+      <input type="text" v-model="jogador.nome" placeholder="Nome do Jogador" />
+      <select v-model="jogador.timeId">
+        <option v-for="time in times" :key="time.id" :value="time.id">
+          {{ time.nome }}
+        </option>
+      </select>
+      <button @click="salvar">Salvar</button>
+    </div>
+    <div class="list-jogadores">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Time</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="jogador in jogadores" :key="jogador.id">
+            <td>{{ jogador.id }}</td>
+            <td>{{ jogador.nome }}</td>
+            <td>{{ jogador.time.nome }}</td>
+            <td>??</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
+
+<style>
+.title {
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+}
+.form-input {
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+}
+
+.form-input input {
+  width: 60%;
+  height: 45px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 0 10px;
+}
+
+.form-input button {
+  margin-left: 1%;
+  width: 15%;
+  height: 45px;
+  border: 1px solid rgb(36, 127, 65);
+  border-radius: 10px;
+  background-color: rgb(36, 127, 65);
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.list-jogadores {
+  display: flex;
+  justify-content: center;
+}
+
+table {
+  width: 50%;
+  margin: 2% auto;
+  border-collapse: collapse;
+}
+
+table tr th {
+  border: 1px solid #ccc;
+  padding: 10px;
+  font-weight: bold;
+}
+
+table tr td {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+table tr:nth-child(odd) {
+  background-color: #ccc;
+}
+</style>
